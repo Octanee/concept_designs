ac6d78c385144d0c25584a04d054624b8148f31a
Kamil Boryczka <leoctaneee@gmail.com>
2023-01-12 18:21:44 +0100

    Observe bloc changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
-import 'package:concept_designs/src/app.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+import 'package:concept_designs/src/app.dart';
+import 'package:concept_designs/src/app_bloc_observer.dart';
+import 'package:concept_designs/src/common.dart';
+
+Future<void> main() async {
+  FlutterError.onError = (details) =>
+      logger.e(details.exceptionAsString(), details.exception, details.stack);
+
+  Bloc.observer = AppBlocObserver();
+
+  runZonedGuarded(
+    () => runApp(const MyApp()),
+    (error, stack) => logger.e(error.toString(), error, stack),
+  );
 }
